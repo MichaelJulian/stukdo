e27d9d0b05b326843b2986b4466c0a97f67e53e3
MichaelJulian <mikeyjmikeyj@gmail.com>
2015-10-03 00:01:31 -0700

    add header

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
  root 'pages#home'
- get 'about' => "pages#about"
+ get 'about' => "pages#about" 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
